bb7b679b1ae9bb97ebf3e3f416002a22ae45ec9a
Winnerawan T <third1337@gmail.com>
2021-04-01 16:40:26 +0700

    .

---
+++ ByteToHex.java
@@ -0,0 +1,22 @@
+import java.nio.charset.StandardCharsets;
+
+public class ByteToHex {
+
+    public static String hex(byte[] bytes) {
+        StringBuilder result = new StringBuilder();
+        for (byte aByte : bytes) {
+            result.append(String.format("%02x", aByte));
+            // upper case
+            // result.append(String.format("%02X", aByte));
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+
+        String input = ".";
+        System.out.println(hex(input.getBytes(StandardCharsets.UTF_8)));
+
+  
+	}
+}
